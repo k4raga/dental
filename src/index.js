9d3152f02c2c9e8e457f9ed87a254183ef4f86de
k4raga <illkiri02@mail.ru>
2023-05-30 11:26:49 +0300

    restore repo

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./index.html";
+
+import "./less/const.less";
+import "./less/global.less";
+import "./less/style.less";
+
+import "./less/blocks/header.less";
+import "./less/blocks/mainBanner.less";
+import "./less/blocks/features.less";
+import "./less/blocks/consult.less";
+import "./less/blocks/about.less";
+import "./less/blocks/points.less";
+import "./less/blocks/trust.less";
+import "./less/blocks/services.less";
